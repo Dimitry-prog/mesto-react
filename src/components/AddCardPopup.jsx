fd48f52239e307ea16d682692da556e002566fdd
Dimitry-prog <kfifa3@gmail.com>
2022-10-19 22:00:51 +0300

    feat: validation AddCard

--- src/components/AddCardPopup.jsx
+++ src/components/AddCardPopup.jsx
@@ -1,18 +1,22 @@
-import React, { useState} from "react";
+import React, {useEffect, useState} from "react";
 import PopupWithForm from "./PopupWithForm";
 import {useAppContext} from "../context/AppContext";
 import {api} from "../utils/Api";
 
 const AddCardPopup = () => {
   const {cards, setCards, handleClosePopups, isAddPlacePopupOpen} = useAppContext();
-  const [place, setPlace] = useState('');
-  const [link, setLink] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+  const [inputValues, setInputValues] = useState({
+    place: '',
+    link: ''
+  });
+  const [isValidForm, setIsValidForm] = useState(false);
+  const [errors, setErrors] = useState({});
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setIsLoading(true);
-    api.postNewCard(place, link)
+    api.postNewCard(inputValues.place, inputValues.link)
       .then(newCard => {
         setCards([newCard, ...cards]);
       })
@@ -24,6 +28,37 @@ const AddCardPopup = () => {
     handleClosePopups();
   }
 
+  const validation = (inputValue) => {
+    const errorMessage = {};
+    if(!inputValue.place) {
+      errorMessage.place = 'Заполните поле';
+    } else if (inputValue.place.length < 3 || inputValue.place.length > 30) {
+      errorMessage.place = 'Поле должно содержать минимум 3 символа, но не более 30';
+    }
+    if(!inputValue.link) {
+      errorMessage.link = 'Заполните поле';
+    } else if (!inputValue.link.startsWith('https://')) {
+      errorMessage.link = 'Введите ссылку';
+    }
+
+    return errorMessage;
+  }
+
+  const handleChange = (e) => {
+    setInputValues({...inputValues, [e.target.name]: e.target.value});
+  }
+
+  useEffect(() => {
+
+    setErrors(validation(inputValues));
+    if (Object.values(errors).length === 0) {
+      setIsValidForm(true);
+    } else {
+      setIsValidForm(false);
+    }
+
+  }, [inputValues, Object.values(errors).length])
+
   return (
     <PopupWithForm
       name="card"
@@ -31,11 +66,12 @@ const AddCardPopup = () => {
       isOpenPopup={isAddPlacePopupOpen}
       submitText={isLoading ? "Сохрание..." : "Создать"}
       onSubmit={handleSubmit}
+      isValidForm={isValidForm}
     >
       <label className="form__label">
         <input
-          value={place}
-          onChange={(e) => setPlace(e.target.value)}
+          value={inputValues.place}
+          onChange={handleChange}
           type="text"
           className="input form__input form__input_type_place"
           name="place"
@@ -44,19 +80,19 @@ const AddCardPopup = () => {
           id="place"
           minLength="2"
           maxLength="30"/>
-        <span className="form__error-message place-error"></span>
+        <span className="form__error-message form__error-message_active place-error">{errors.place}</span>
       </label>
       <label className="form__label">
         <input
-          value={link}
-          onChange={(e) => setLink(e.target.value)}
+          value={inputValues.link}
+          onChange={handleChange}
           type="url"
           className="input form__input form__input_type_link"
           name="link"
           placeholder="Ссылка на картинку"
           required
           id="link"/>
-        <span className="form__error-message link-error"></span>
+        <span className="form__error-message form__error-message_active link-error">{errors.link}</span>
       </label>
     </PopupWithForm>
   );
